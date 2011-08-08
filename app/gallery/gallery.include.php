cc2ad8c799f54ec197aa8d6b1d543e67127f1f17
Petr Babicka <babcca@gmail.com>
2011-08-08 03:47:57 +0200

    gallery administration

--- app/gallery/gallery.include.php
+++ app/gallery/gallery.include.php
@@ -6,4 +6,26 @@
 								"class"=>"gallery",
 								"method"=>"generate",
 								"params"=>array("text_id"=>"%([0-9]+)")));
+	
+	Presenter::view("gallery", array(
+								"class"=>"gallery",
+								"method"=>"gallery_editor",
+								"params"=>array()
+								));
+								
+	Presenter::view("gallery", array(
+								"class"=>"gallery_model",
+								"method"=>"upload_image",
+								"params"=>array("new_image_desc"=>"%([^<>]+)")
+								));
+	Presenter::view("gallery", array(
+								"class"=>"gallery_model",
+								"method"=>"update_desc",
+								"params"=>array("image_id"=>"%([0-9]+)", "desc"=>"%([^<>]+)")
+								));
+	Presenter::view("gallery", array(
+								"class"=>"gallery_model",
+								"method"=>"delete_image",
+								"params"=>array("image_id"=>"%([0-9]+)")
+								));
 ?>
