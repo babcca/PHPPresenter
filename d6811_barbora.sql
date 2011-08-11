f484abb80dad85b32cca299ae0bc0cb44701f5c6
Petr Babicka <babcca@gmail.com>
2011-08-11 07:49:39 +0200

    new db scheme for booking

--- d6811_barbora.sql
+++ d6811_barbora.sql
@@ -5,23 +5,41 @@ SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `clanek`;
-CREATE TABLE `clanek` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `str_id` char(100) COLLATE utf8_czech_ci NOT NULL,
-  `lang` char(2) COLLATE utf8_czech_ci NOT NULL,
-  `template` char(100) COLLATE utf8_czech_ci NOT NULL,
-  `content_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+DROP TABLE IF EXISTS `book_customer`;
+CREATE TABLE `book_customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8_bin NOT NULL,
+  `phone` varchar(20) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `clanek` (`id`, `str_id`, `lang`, `template`, `content_id`) VALUES
-(1000,	'lage',	'de',	'content.tpl',	1),
-(1001,	'location',	'en',	'content.tpl',	2),
-(1002,	'home',	'en',	'content.tpl',	3),
-(1003,	'home',	'de',	'content.tpl',	1),
-(1004,	'contact-us',	'en',	'contact.tpl',	1),
-(1005,	'gallery',	'en',	'gallery.tpl',	1);
+
+DROP TABLE IF EXISTS `book_order`;
+CREATE TABLE `book_order` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `date_from` char(10) COLLATE utf8_bin NOT NULL,
+  `date_to` char(10) COLLATE utf8_bin NOT NULL,
+  `transfer` char(5) COLLATE utf8_bin NOT NULL,
+  `breakfast` char(5) COLLATE utf8_bin NOT NULL,
+  `arrival_time` int(11) NOT NULL,
+  `message` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+DROP TABLE IF EXISTS `book_rooms`;
+CREATE TABLE `book_rooms` (
+  `room_id` int(11) NOT NULL DEFAULT '0',
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `guests` int(11) NOT NULL,
+  `beds_s` int(11) NOT NULL,
+  `beds_d` int(11) NOT NULL,
+  `parking` char(5) COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 
 DROP TABLE IF EXISTS `contact_contacts`;
 CREATE TABLE `contact_contacts` (
@@ -36,6 +54,14 @@ CREATE TABLE `contact_contacts` (
 INSERT INTO `contact_contacts` (`id`, `mobile_phone`, `telephone`, `fax`, `email`) VALUES
 (1,	'+420 777 188 188',	'+420 261 227 635',	'',	'ubytovani@omikron.cz ');
 
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_path` varchar(40) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
 DROP TABLE IF EXISTS `gallery`;
 CREATE TABLE `gallery` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -43,19 +69,14 @@ CREATE TABLE `gallery` (
   `big` varchar(100) COLLATE utf8_bin NOT NULL,
   `desc` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `gallery` (`id`, `small`, `big`, `desc`) VALUES
-(26,	'gallery_images//0db31d632f84b5cc330487dae134192b.jpg',	'gallery_images//0db31d632f84b5cc330487dae134192b.jpg',	'popis 7'),
-(25,	'gallery_images//adc8091c1ccde17da076c53142c14501.jpg',	'gallery_images//adc8091c1ccde17da076c53142c14501.jpg',	'Popis 5'),
-(22,	'gallery_images//e5b2072caf6f08aa9d026bb224a1d2f8.jpg',	'gallery_images//e5b2072caf6f08aa9d026bb224a1d2f8.jpg',	'Popis 2'),
-(23,	'gallery_images//74b1dd58af8d8908c791aa4cf3d808db.jpg',	'gallery_images//74b1dd58af8d8908c791aa4cf3d808db.jpg',	'Popis 3'),
-(24,	'gallery_images//9e8bafd522d3de5fef857a0be6971c23.jpg',	'gallery_images//9e8bafd522d3de5fef857a0be6971c23.jpg',	'Popis 4'),
-(21,	'gallery_images//f3b5a8d9bd09dd1ec693ddddfd1f21b5.jpg',	'gallery_images//f3b5a8d9bd09dd1ec693ddddfd1f21b5.jpg',	'Popis 1'),
-(27,	'gallery_images//759e2caf6fc3bf58d906a3d8538c69fe.jpg',	'gallery_images//759e2caf6fc3bf58d906a3d8538c69fe.jpg',	'Popis 8'),
-(28,	'gallery_images//9d28cb20ed4ae0e31ab7411ecdd53c36.jpg',	'gallery_images//9d28cb20ed4ae0e31ab7411ecdd53c36.jpg',	'popis 9'),
-(29,	'gallery_images//4b9c89babbbf164e99528c2019cefe47.jpg',	'gallery_images//4b9c89babbbf164e99528c2019cefe47.jpg',	'popis 10'),
-(30,	'gallery_images//61d952211151f92070cbbcc546b6000b.jpg',	'gallery_images//61d952211151f92070cbbcc546b6000b.jpg',	'popis 12'),
+(36,	'gallery_images/a3171a2d098f14ceb1f64a65a34e2eef.jpg',	'gallery_images/a3171a2d098f14ceb1f64a65a34e2eef.jpg',	'Popis 9'),
+(27,	'gallery_images//759e2caf6fc3bf58d906a3d8538c69fe.jpg',	'gallery_images//759e2caf6fc3bf58d906a3d8538c69fe.jpg',	'Popiska'),
+(35,	'gallery_images/70a3bddc8002543a9b77ad12e1f99cf3.jpg',	'gallery_images/70a3bddc8002543a9b77ad12e1f99cf3.jpg',	'popisek 6'),
+(34,	'gallery_images/f1f99c0aaf5b3f4c6a9e3a1dcbd2569d.jpg',	'gallery_images/f1f99c0aaf5b3f4c6a9e3a1dcbd2569d.jpg',	'popis 11'),
+(32,	'gallery_images/ef5be9d5860f5305a97430d799498ad9.jpg',	'gallery_images/ef5be9d5860f5305a97430d799498ad9.jpg',	'popisek'),
 (31,	'gallery_images//6b9d6b441a0e1b7e9de0044433d66c65.jpg',	'gallery_images//6b9d6b441a0e1b7e9de0044433d66c65.jpg',	'popis 13');
 
 DROP TABLE IF EXISTS `page_content`;
@@ -68,13 +89,13 @@ CREATE TABLE `page_content` (
 
 INSERT INTO `page_content` (`id`, `title`, `text`) VALUES
 (1,	'About us',	'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a ipsum augue. Vivamus cursus vulputate mi. Nulla pharetra lectus id libero vehicula vel venenatis magna dictum. Aenean feugiat ligula magna, sit amet suscipit enim. Duis vestibulum nunc vitae lorem egestas feugiat vel at felis. Suspendisse blandit, est id imperdiet venenatis, turpis metus interdum purus, consequat vehicula orci sapien in neque. Praesent vulputate aliquet velit consequat tempor. Praesent feugiat interdum dui, sit amet ornare dui rutrum quis. Mauris hendrerit cursus risus, quis adipiscing ante tristique sit amet. Duis diam felis, consequat eu ornare nec, rhoncus non massa. Duis dui ligula, ornare a faucibus ac, pulvinar sed sapien. Vivamus rhoncus lacinia eros, in accumsan sapien iaculis in. Maecenas sit amet lorem at ipsum tincidunt fringilla.</p>'),
-(2,	'O nas',	'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a ipsum augue. Vivamus cursus vulputate mi. Nulla pharetra lectus id libero vehicula vel venenatis magna dictum. Aenean feugiat ligula magna, sit amet suscipit enim. Duis vestibulum nunc vitae lorem egestas feugiat vel at felis. Suspendisse blandit, est id imperdiet venenatis, turpis metus interdum purus, consequat vehicula orci sapien in neque. Praesent vulputate aliquet velit consequat tempor. Praesent feugiat interdum dui, sit amet ornare dui rutrum quis. Mauris hendrerit cursus risus, quis adipiscing ante tristique sit amet. Duis diam felis, consequat eu ornare nec, rhoncus non massa. Duis dui ligula, ornare a faucibus ac, pulvinar sed sapien. Vivamus rhoncus lacinia eros, in accumsan sapien iaculis in. Maecenas sit amet lorem at ipsum tincidunt fringilla.</p>\r\n<p>Aenean euismod diam ut tortor scelerisque dapibus. Duis porta porta ligula, eu aliquet erat commodo ac. Donec et tortor tortor, eu tincidunt urna. Sed malesuada rutrum venenatis. Fusce erat erat, posuere in vulputate fermentum, cursus ut tortor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse malesuada nunc in massa tincidunt interdum. Pellentesque a ligula dui. Etiam nec enim orci, vitae pulvinar dui. Pellentesque laoreet ullamcorper libero nec tincidunt. Proin hendrerit consectetur elit a vestibulum. Proin aliquet, orci eget consectetur adipiscing, sem quam facilisis eros, et pellentesque massa nulla quis urna. Maecenas tempus suscipit leo mollis dapibus. Nulla ut odio vel nunc euismod pharetra sit amet et tortor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>'),
+(2,	'Pension location',	'<p>Our Apartments are located in very quiet and natural part of Prague, just 15 minutes from town centre! Very near (about 6 minutes) is Metro station &bdquo;Pražsk&eacute;ho povst&aacute;n&iacute;&ldquo; which is on red line C. Our apartments are also close to other mean of public transport &ndash; Tram. You can use tram to get directly to Charles Bridge (about 12 minutes with Tram) from station Podolsk&aacute; Vod&aacute;rna. So you can choose between two means of public Transport to get into prague centre.</p>\r\n<p>Address: Doudova 22, Prague 4 (6 minutes from Metro station Pražsk&eacute;ho Povst&aacute;n&iacute;)</p>\r\n<p>Some of our guests even wonder if they are really in Prague, but when they see the beautiful view from windows and balcony which is in most of our apartments, they quickly realise how close to the heart of town they are. Directly from most of apartments you can see one of Prague landmarks &ndash; Vy&scaron;ehrad.</p>\r\n<div class=\"page_accordion accordion\">\r\n<h3><a href=\"#\">How to reach us from the Metro station Pražsk&eacute;ho Povst&aacute;n&iacute;?</a></h3>\r\n<div>\r\n<p>zde by měla b&yacute;t mapa cesty na metro (map) + popisek: The way from Metro is very simple. When you arrive at Pražsk&eacute;ho povst&aacute;n&iacute;, come out to the left direction &bdquo;Lomnick&eacute;ho&ldquo; exit. You will see huge CSOB bank in front of you and slightly on the left side, theres a main street tilted down a bit. Follow this street and turn the 3rd street left. Then go stright and you will see our Vila in front of you. Its really simple as you can see on the map. You can also view the map of prague Metro. (odkaz na mapu metra se zakroužkovanou stan&iacute;c&iacute; metra &bdquo;pražsk&eacute;ho povst&aacute;n&iacute;&ldquo;</p>\r\n</div>\r\n<h3><a href=\"#\">How to reach us from the Prague Airport - Ruzyně?</a></h3>\r\n<div>\r\n<p>If you dont use taxi or our transport(hyperlink na str&aacute;nku Apartments, kde budou rozeps&aacute;ny i služby včetně transportu z leti&scaron;tě), you can also use public transport, first you will need to use Bus, and then Metro. The whole way takes about 35 minutes. So first, get on bus 119 which starts in the airport area. Get to station Dejvick&aacute;, there is Metro station on green line &bdquo;A&ldquo;. Then you will need to change for the red line &bdquo;C&ldquo; in station Muzeum. From there just go in direction H&aacute;je to station Pražsk&eacute;ho Povst&aacute;n&iacute; and within few minutes you will reach our Vila. If you use taxi, it should cost about 30&euro; from airport to our Apartments, dont forget to give them our address &ndash; Doudova 22, Praha 4</p>\r\n</div>\r\n<h3><a href=\"#\">How to reach us if you travel by a train?</a></h3>\r\n<div>\r\n<p>Thats really simple, because Prague Main Railway Station (Praha-Hlavn&iacute; N&aacute;draž&iacute;) has its own Metro station which is on the same line (&bdquo;C&ldquo; - red line) as station Pražsk&eacute;ho Povst&aacute;n&iacute;. So only thing that you need to know is that you have to go in the direction H&aacute;je and in about 8 minutes you will be in our Metro station Pražsk&eacute;ho Povst&aacute;n&iacute;. From there its only 6 minutes to our Vila.</p>\r\n</div>\r\n<h3><a href=\"#\">How to reach us if you arrive by a car?</a></h3>\r\n<div>\r\n<p>Well, if you use GPS navigation, its simple &ndash; type in adress &bdquo;Doudova 22, Prague&ldquo;. If you dont, its not simple less. Our Apartments are located just 3 minutes (by car) from Highway 5. Května (D1 Praha &ndash; Brno). The best thing is, that the highway is close, but not close enought to make disturbing noise, also there is a speed limit of 50 km/h. You can see the Highway in</p>\r\n<p>this map (odkaz na malou mapku s metrem + d&aacute;lnic&iacute;)</p>\r\n</div>\r\n<h3><a href=\"#\">How to get into centre?</a></h3>\r\n<div>\r\n<p>There are two very good way how to get into town centre which are fast and comfortable. First one is Metro (station Pražsk&eacute;ho &ndash; 6 minutes from apartments), from there just go to station Museum and you will come out in Venceslav Square in front of National Museum. The second one is Tram (station Podolsk&aacute; Vod&aacute;rna &ndash; 7 minutes form apartments), from there you will have beautiful way next to river Moldau, you will see Prague Castle. The 4th station is called &bdquo;N&aacute;rodn&iacute; divadlo&ldquo;, which in Czech means National Theater, and thats exactly where you will appear. And its just few steps away from Charles Bridge. When you arrive, we will show you the best ways by ourself, so dont worry that you will have any problems getting into centre. Its really easy. During your stay if you get lost, you can simply call our phone number and we will try to help you.</p>\r\n</div>\r\n</div>'),
 (3,	'Home',	'home page'),
-(5,	'Gallerie obrazku',	'Uvodni text k galerii obrazku'),
+(5,	'Booking apartments',	'<p>This only <strong>preliminary apartments&nbsp;book</strong>, for complete booking we will contact you by your contact email.&nbsp;In this computed price maybe small different.</p>'),
 (6,	'Kontakni informace - Misto pro Vas dotaz',	'Pokud máte jakýkoli dotaz ohledně ubytování ve vile Barbora v Praze, prosím neváhejte nám napsat nebo zatelefonovat. Děkujeme a těšíme se na osobní setkání s vámi.'),
 (7,	'Galerie obrazku',	'Vitejte v galerii obrazku'),
-(8,	'Home',	'home page'),
-(9,	'title',	'<p>content2222</p>'),
+(8,	'Home',	'<p>home page :)</p>'),
+(9,	'title',	'<p>content</p>'),
 (10,	'title',	'<p>content</p>'),
 (11,	'title',	'content'),
 (12,	'title',	'content2'),
@@ -107,4 +128,4 @@ INSERT INTO `presenter` (`id`, `uri`, `position`, `title`, `lang`, `app`, `metho
 (13,	'buchung',	4,	'Buchung Wohnung',	'de',	'book',	'book_form',	'text_id=11'),
 (14,	'kontaktieren-uns',	6,	'Kontaktieren Sie uns',	'de',	'contact',	'contact_us',	'text_id=12');
 
--- 2011-08-08 04:07:59
+-- 2011-08-11 07:46:48
