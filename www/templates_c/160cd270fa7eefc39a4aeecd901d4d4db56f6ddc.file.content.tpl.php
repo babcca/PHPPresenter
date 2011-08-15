4311004244b6e7104e1cbfeb1cc3174ae890798d
Martin Kolesar <Mato@.(none)>
2011-08-16 01:31:57 +0200

    footer

---
+++ www/templates_c/160cd270fa7eefc39a4aeecd901d4d4db56f6ddc.file.content.tpl.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version Smarty-3.0.7, created on 2011-08-15 22:53:31
+         compiled from "H:\Mago\WebPages\Pension_Barbora\web\lib/../app/page/templates\content.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:251174e49874be41d44-87236168%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '160cd270fa7eefc39a4aeecd901d4d4db56f6ddc' => 
+    array (
+      0 => 'H:\\Mago\\WebPages\\Pension_Barbora\\web\\lib/../app/page/templates\\content.tpl',
+      1 => 1312329801,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '251174e49874be41d44-87236168',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<h1><?php echo $_smarty_tpl->getVariable('title')->value;?>
+</h1>
+<div>
+<?php echo $_smarty_tpl->getVariable('text')->value;?>
+
+</div>
